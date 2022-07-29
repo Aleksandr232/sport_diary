7f1440d5044786df1f2bccf68d4799495d464dac
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-29 13:44:07 +0300

    hooks useContext

--- src/components/auth.js
+++ src/components/auth.js
@@ -7,8 +7,8 @@ export default function Auth() {
         <div className="max-w-md w-full space-y-8">
           <div>
             <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              className="mx-auto h-20 w-auto"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Indoor_rowing_pictogram.svg/2048px-Indoor_rowing_pictogram.svg.png"
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Войти в аккаунт</h2>
@@ -67,7 +67,7 @@ export default function Auth() {
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  <LockClosedIcon className="h-5 w-5 text-black group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
                Войти
               </button>
