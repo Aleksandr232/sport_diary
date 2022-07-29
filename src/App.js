5d50f28c5ba6aae6189c13a1676fdce4c0175ba8
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-29 14:32:37 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React,{useState} from "react";
 import Auth from "./components/auth";
+import Main from "./components/main";
+import { AuthContext}  from "./authContext";
+
 
 
 function App() {
+  const[isAuth, setIsAuth]=useState('')
   return (
-    <>
-      <Auth/>
-    </>
+    <AuthContext.Provider value={{isAuth, setIsAuth}}>
+      <div>
+        {!isAuth ? <Auth/> : <Main/>}
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/auth.js
+++ src/components/auth.js
@@ -10,10 +10,10 @@ export default function Auth() {
 
     const authHandler=()=>{
         if(email && password){
-            if(email !== 'row@232.ru'){
+            if(email == 'row@232.ru'){
                 return setError('не верная почта')
             }
-            if(password !== '12345'){
+            if(password == '12345'){
                 return setError('не верный пароль')
             }
     
@@ -39,21 +39,14 @@ export default function Auth() {
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Войти в аккаунт</h2>
             <p className="mt-2 text-center text-sm text-gray-600"></p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
+          <div className="mt-8 space-y-6" >
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
                 <input
-                  value={email}
-                  onChange={setEmail}
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
+                 onChange={setEmail}
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="почта"
                 />
@@ -63,13 +56,7 @@ export default function Auth() {
                   Password
                 </label>
                 <input
-                  value={password}
-                  onChange={setPassword}
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
+                onChange={setPassword}
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="пароль"
                 />
@@ -102,7 +89,7 @@ export default function Auth() {
                Войти
               </button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
