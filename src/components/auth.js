b91d21c5e4a566c68a2328acde52ddc682602d03
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-29 13:48:38 +0300

    hooks

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,6 +1,11 @@
 import { LockClosedIcon } from '@heroicons/react/solid'
+import { useAuth } from '../hooks/useAuth'
+import { useState } from 'react'
 
 export default function Auth() {
+    const[email, setEmail]=useState('')
+    const[password, setPassword]=useState('')
+
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
