63543c587bf394196e74208ff1485d5e227eab7e
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-29 16:47:00 +0300

    main

--- src/components/main.js
+++ src/components/main.js
@@ -39,13 +39,6 @@ export default function Main() {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="hidden   lg:block  bg-white h-12 w-auto"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Indoor_rowing_pictogram.svg/2048px-Indoor_rowing_pictogram.svg.png"
-                    alt="Workflow"
-                  />
-                </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
