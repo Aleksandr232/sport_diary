341f9a9364e7b92445ff1fcfdc7ce2507b65cc96
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-08 14:48:37 +0300

    records

--- src/pages/records.js
+++ src/pages/records.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Main from '../components/main'
+import Tables from "../components/table";
 
 
 export default function Records(){
+    
     return(
         <div>
             <Main/>
+            <Tables/>
+
         </div>
     )
 }
