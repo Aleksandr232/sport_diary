f59b8f18c2f767193ce2d25c4794863ae836216a
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-08 16:32:02 +0300

    data

---
+++ src/components/data.js
@@ -0,0 +1,444 @@
+
+export const DATA = [
+    
+                {
+                    "name": "Тиган Фарли",
+                    "gender":"woman",
+                    "category": "USA",
+                    "value": "7: 25,2",
+                    "seson":"2021 г",
+                    "age":"12 лет и младше"
+                },
+                {
+                    "name": "Грейс МакГинли",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "6: 46,1",
+                    "seson":"2016 г",
+                    "age": "15–16"
+                    
+                },
+                {
+                    "name": "Кристин Кавалло",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "7: 05,7",
+                    "seson":"2013 г",
+                    "age":"17–18",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Брук Муни",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "6: 21,1",
+                    "seson":"2021 г",
+                    "age":"19-29",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Лиза Шленкер",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "6: 56,7",
+                    "seson":"2000 г",
+                    "age":"30-39",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Кэрол Скрики",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "6: 48,2",
+                    "seson":"2003 г",
+                    "age":"40-49",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Джоан Ван Блом",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "7: 30,6",
+                    "seson":"2008 г",
+                    "age":"55-59",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Сьюзан Хутен",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "7: 35,5",
+                    "seson":"2015 г",
+                    "age":"60-64",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Сьюзан Хутен",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "7: 44,6",
+                    "seson":"2018 г",
+                    "age":"65-69",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Сьюзан Хутен",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "7: 40,5",
+                    "seson":"2018 г",
+                    "age":"65-69",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Луанн Миллс",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "8: 12,7",
+                    "seson":"2009 г",
+                    "age":"70-74",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Луанн Миллс",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "8: 34,7",
+                    "seson":"2014 г",
+                    "age":"75-79",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Луанн Миллс",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "8: 47,5",
+                    "seson":"2019 г",
+                    "age":"85-89",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "МэриЭллен Мур",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "10: 23,6",
+                    "seson":"2018 г",
+                    "age":"30-39",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Эрнестин Байер",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "12: 07,5",
+                    "seson":"2000 г",
+                    "age":"90-94",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Дороти (Дотти) Стюарт",
+                    "category": "USA",
+                    "gender":"woman",
+                    "value": "14: 23,1",
+                    "seson":"2018 г",
+                    "age":"95-99",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Мэтт Филлипс",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 57,8",
+                    "seson":"2017 г",
+                    "age":"12 лет и младше"
+                    
+                },
+                {
+                    "name": "Исайя Харрисон",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 16,7",
+                    "seson":"2018 г",
+                    "age":"13–14"
+                   
+                },
+                {
+                    "name": "Исайя Харрисон",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "5: 53,2",
+                    "seson":"2020 г",
+                    "age":"15–16"
+                    
+                },
+                {
+                    "name": "Андрей Бенко",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 01,7",
+                    "seson":"2018",
+                    "age":"50-54",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Майк Смит",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 29,6",
+                    "seson":"2018",
+                    "age":"50-59",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Томас Дарлинг",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 12,6",
+                    "seson":"2014 г",
+                    "age":"50-59",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Томас Дарлинг",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 20,7",
+                    "seson":"2019",
+                    "age":"60-64",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Роджер Борггаард",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "6: 52,7",
+                    "seson":"2016",
+                    "age":"70-74",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Роберт Спенгер",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "8: 13,7",
+                    "seson":"2010",
+                    "age":"85-89",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Роберт Спенгер",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "8: 44,7",
+                    "seson":"2015",
+                    "age":"90-94",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Роберт Спенгер",
+                    "category": "USA",
+                    "gender":"men",
+                    "value": "8: 42,4",
+                    "seson":"2015",
+                    "age":"90-94",
+                    "weight":"hwt"
+                },
+                
+            
+        
+        
+            {
+                    "name": "Бекки Ли",
+                    "category": "NZL",
+                    "gender":"woman",
+                    "value": "7: 06,1",
+                    "seson":"2016",
+                    "age":"13-14"
+                    
+                },
+                {
+                    "name": "Роб Уодделл",
+                    "category": "NZL",
+                    "gender":"men",
+                    "value": "5: 36,6",
+                    "seson":"2008",
+                    "age":"30-39",
+                    "weight":"hwt"
+                },
+               
+            
+        
+        
+        
+            {
+                "name": "Жюстин Рестон",
+                "category": "GBR",
+                "gender":"woman",
+                "value": "7: 18,1",
+                "seson":"2020",
+                "age":"50-54",
+                "weight":"lwt"
+                
+            },
+            {
+                "name": "Валери Коулман",
+                "category": "GBR",
+                "gender":"woman",
+                "value": "13: 05,0",
+                "seson":"2021 г",
+                "age":"90-94",
+                "weight":"hwt"
+            },
+            {
+                "name": "Роджер Бангай",
+                "category": "GBR",
+                "gender":"men",
+                "value": "7: 24,1",
+                "seson":"2012 г",
+                "age":"75-79",
+                "weight":"lwt"
+            },  
+            {
+                "name": "Джеффри Найт",
+                "category": "GBR",
+                "gender":"men",
+                "value": "7: 40,4",
+                "seson":"2015 г",
+                "age":"80-84",
+                "weight":"lwt"
+            },
+            {
+                "name": "Майк Херли",
+                "category": "GBR",
+                "gender":"men",
+                "value": "7: 54,2",
+                "seson":"2021 г",
+                "age":"85-89",
+                "weight":"hwt"
+            },
+            {
+                "name": "Джон Ходжсон",
+                "category": "GBR",
+                "gender":"men",
+                "value": "10: 28,2",
+                "seson":"2006 г",
+                "age":"95-99",
+                "weight":"lwt"
+            },
+            {
+                "name": "Джон Ходжсон",
+                "category": "GBR",
+                "gender":"men",
+                "value": "13: 32,6",
+                "seson":"2011 г",
+                "age":"100+",
+                "weight":"lwt"
+            },
+
+                
+            
+        
+        
+            
+                {
+                    "name": "Энн Крессвелл",
+                    "category": "AUS",
+                    "gender":"woman",
+                    "value": "9: 07,4",
+                    "seson":"2019 г",
+                    "age":"80-84",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Джош Данкли-Смит",
+                    "category": "AUS",
+                    "gender":"men",
+                    "value": "5: 35,8",
+                    "seson":"2018 г",
+                    "age":"19-29",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Род Стюарт",
+                    "category": "AUS",
+                    "gender":"men",
+                    "value": "6: 59,9",
+                    "seson":"2020 г",
+                    "age":"70-74",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Пол Гест",
+                    "category": "AUS",
+                    "gender":"men",
+                    "value": "7: 26,9",
+                    "seson":"2019 г",
+                    "age":"80-84",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Винс Хоум",
+                    "category": "AUS",
+                    "gender":"men",
+                    "value": "10: 49,7",
+                    "seson":"2021 г",
+                    "age":"95-99",
+                    "weight":"lwt"
+                },
+                
+                
+            
+        
+        
+            
+                {
+                    "name": "Дженнифер Кэссон",
+                    "category": "CAN",
+                    "gender":"woman",
+                    "value": "6: 53,8",
+                    "seson":"2019 г",
+                    "age":"19-29",
+                    "weight":"lwt"
+                },
+                {
+                    "name": "Джоан Ричи",
+                    "category": "CAN",
+                    "gender":"woman",
+                    "value": "6: 58,0",
+                    "seson":"2012 г",
+                    "age":"50-54",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Анн Бурлиу",
+                    "category": "CAN",
+                    "gender":"woman",
+                    "value": "7: 10,1",
+                    "seson":"2016 г",
+                    "age":"55-59",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Алида Кингсвуд",
+                    "category": "CAN",
+                    "gender":"woman",
+                    "value": "10: 11,8",
+                    "seson":"2021 г",
+                    "age":"85-89",
+                    "weight":"hwt"
+                },
+                {
+                    "name": "Гюстав Карл Шох",
+                    "category": "CAN",
+                    "gender":"men",
+                    "value": "6: 33,5",
+                    "seson":"2021 г",
+                    "age":"65-69",
+                    "weight":"hwt"
+                }
+                
+
+            
+        
+        
+    ]
--- src/components/table.js
+++ src/components/table.js
@@ -1,46 +1,37 @@
 import React,{useState} from "react";
+import {DATA} from "./data";
 
 
 export default function Tables(){
-    const [data, setData] = useState('')
+    const [open, setOpen]= useState(false)
+
+   
+
     return(
-        <table className="table-fixed m-auto mt-20  ">
-  <thead>
-    <tr>
-      <th>Возраст</th>
-      <th>Масса</th>
-      <th>Имя</th>
-      <th>Страна</th>
-      <th>Время</th>
-      <th>Сезон</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>5</td>
-      <td>5</td>
-      <td>5</td>
-      <td>5</td>
-      <td>5</td>
-      <td>5</td>
-    </tr>
-    <tr>
-      <td>r</td>
-      <td>r</td>
-      <td>r</td>
-      <td>r</td>
-      <td>r</td>
-      <td>r</td>
-    </tr>
-    <tr>
-      <td>t</td>
-      <td>g</td>
-      <td>g</td>
-      <td>g</td>
-      <td>g</td>
-      <td>g</td>
-    </tr>
-  </tbody>
-</table>
+     <>
+     <button onClick={()=>setOpen(prev=>!prev)} className="py-2 px-4 border m-20 ml-[45%] bg-blue-400 ">{open ? "Скрыть" : "Показать"}</button>
+        {open && <table  className="table-fixed m-auto mt-20 w-[700px]">
+    <thead>
+        <tr>
+        <th>Возраст</th>
+        <th>Масса</th>
+        <th>Имя</th>
+        <th>Страна</th>
+        <th>Время</th>
+        <th>Сезон</th>
+        </tr>
+    </thead>
+    <tbody>
+    {DATA.map((row, idx)=>( <tr>
+        <td>{row.age}</td>
+        <td>{row.weight}</td>
+        <td>{row.name}</td>
+        <td>{row.category}</td>
+        <td>{row.value}</td>
+        <td>{row.seson}</td>
+        </tr>))}
+    </tbody>
+    </table>}
+        </>
     )
 }
