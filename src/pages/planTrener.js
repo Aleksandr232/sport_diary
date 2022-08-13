25ac62d9c3e4839198948ec5dc49a119f24c02be
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-14 00:05:31 +0300

    plan

--- src/pages/planTrener.js
+++ src/pages/planTrener.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import Main from "../components/main";
 
 
 export default function PlanTrener(){
+    const [plan, setPlan] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    async function fetchUrl() {
+      const response = await fetch('http://localhost:3001/api/contacts', plan);
+      const json = await response.json();
+      console.log(json)
+      setPlan(json);
+      setLoading(false);
+    }
+  
+    useEffect(() => {
+      fetchUrl();
+    }, []);
+    
+  
     return(
-        <div>
+        <>
             <Main/>
-        </div>
+            <h1>{plan?.name}</h1>
+        </>
     )
 }
